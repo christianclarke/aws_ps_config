6072c902b75cee1ad1c9fb9c67d0e3222bf9c910
Christian Clarke <christian.clarke@newvoicemedia.com>
2018-12-17 16:28:29 +0000

    Commandline driven.

--- lib/awsparamstore.go
+++ lib/awsparamstore.go
@@ -1,46 +1,49 @@
-package getconfig
+package awsparamstore
 
 import (
-	"strings"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"strings"
 )
 
 func getSession(awsRegion string) (*session.Session, error) {
 	sess, err := session.NewSessionWithOptions(session.Options{
 		Config:            aws.Config{Region: aws.String(awsRegion)},
 		SharedConfigState: session.SharedConfigEnable,
-		})
-		
+	})
+
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
-		
+
 	return sess, nil
 }
-  
-// Config retrieves everything from parameter store based on region and keyname
-func Config(keyName string, awsRegion string) (map[string]string, error) {
+
+// GetConfig retrieves everything from parameter store based on path and region
+func GetConfig(keyName string, awsRegion string) (map[string]string, error) {
 	sess, err := getSession(awsRegion)
+
+	if err != nil {
+		return nil, err
+	}
+
 	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion(awsRegion))
 	withDecryption := true
 
-    configMap := make(map[string]string)
+	configMap := make(map[string]string)
 	param, err := ssmsvc.GetParametersByPath(&ssm.GetParametersByPathInput{Path: &keyName, WithDecryption: &withDecryption})
-	
-	for ; param.NextToken != nil ; {
+
+	for param.NextToken != nil {
 		if err != nil {
-			logrus.Fatal(err)
 			return nil, err
 		}
 
 		for _, element := range param.Parameters {
-			var key = (*element.Name)[strings.LastIndex(*element.Name, "/") + 1:len(*element.Name)]
+			var key = (*element.Name)[strings.LastIndex(*element.Name, "/")+1 : len(*element.Name)]
 			configMap[key] = *element.Value
 		}
-		
+
 		param, err = ssmsvc.GetParametersByPath(&ssm.GetParametersByPathInput{Path: &keyName, WithDecryption: &withDecryption, NextToken: param.NextToken})
 	}
 
--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
- "aws_ps_config/lib"
- "github.com/sirupsen/logrus"
- "fmt"
+	"aws_ps_config/lib"
+	"flag"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
+var awsParamStorePath string
+var awsRegion string
+var executable string
+
+func init() {
+	flag.StringVar(&awsParamStorePath, "awsParamStorePath", "", "the aws param store path to get")
+	flag.StringVar(&awsRegion, "awsRegion", "", "the aws region param store")
+	flag.StringVar(&executable, "executable", "", "The program to execute")
+	flag.Parse()
+
+	if awsParamStorePath == "" || awsRegion == "" || executable == "" {
+		logrus.Error("The Aws Parameter Store Path, the Aws Region or the executable were not provided.")
+		os.Exit(1)
+	}
+}
+
 func main() {
-	var path = "path"
-	var region = "eu-west-1"
-	
-	x, err := getconfig.Config(path, region)
+	config, err := awsparamstore.GetConfig(awsParamStorePath, awsRegion)
 
 	if err != nil {
 		logrus.Fatal(err)
+		os.Exit(1)
 	}
 
-	fmt.Println(x)
+	fmt.Println(config)
 }
